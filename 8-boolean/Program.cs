406a64e074011057b6bcbcef9bb0558374a371ee
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 12:05:00 -0300

    class 8.5 - more boolean interaction

--- 8-boolean/Program.cs
+++ 8-boolean/Program.cs
@@ -7,11 +7,25 @@ class Program
         Console.WriteLine("Executing project 7 - conditionals");
 
         int johnAge = 16;
-        bool group = true;
+        int guests = 2;
+        bool group = guests > 1;
+
+        string aditionalText;
+
+        if (group == true)
+        {
+            aditionalText = "John is with someone";
+        }
+        else
+        {
+            aditionalText = "John is alone";
+        }
 
         if (johnAge >= 18 || group)
         {
+            Console.WriteLine(aditionalText);
             Console.WriteLine("You may enter!");
+
         }
         else
         {
