95387125cfffe3192bfefdb5350827ef0bae96b6
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 09:21:34 -0300

    class 2 - creating variables

---
+++ 1-helloWorld/2-creatingVariables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Project 2 - Creating Variables");
+
+        int idade;
+
+        idade = 31;
+
+        Console.WriteLine(idade);
+
+        idade = idade + 5;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
