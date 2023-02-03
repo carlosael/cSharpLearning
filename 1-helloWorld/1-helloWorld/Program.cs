65073155a5808cc6275abf1d0aa6214d96ae14f8
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 09:16:50 -0300

    class 1 - hello world

---
+++ 1-helloWorld/1-helloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
