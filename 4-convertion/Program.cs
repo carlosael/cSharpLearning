11bc00307023af11ec2d43382bd15e79755e7e64
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 09:47:44 -0300

    class 4 - convertingVariables

---
+++ 4-convertion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double salary;
+        salary = 3000.15;
+        Console.WriteLine(salary);
+
+
+        int intergerSalary;
+        intergerSalary = (int)salary;
+        Console.WriteLine(intergerSalary);
+
+
+        // Long is a variable that holds 64 bits
+        long x = 200000000000;
+        Console.WriteLine(x);
+
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float height = 1.96f;
+        Console.WriteLine(height);
+
+        Console.WriteLine("Type 'Enter' to close.");
+        Console.ReadLine();
+    }
+}
