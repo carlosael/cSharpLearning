61dbead0ac740fada4b7449063f35a5b3fc08fc5
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 10:10:31 -0300

    class 5 - texts and characters

---
+++ 5-textsAndCaracters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 5 - texts and characters");
+
+        char letter = 'a';
+        Console.WriteLine(letter);
+
+        letter = (char)65;
+        Console.WriteLine(letter);
+
+        letter = (char)(65 + 1);
+        Console.WriteLine(letter);
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
