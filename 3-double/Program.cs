eb6d5829dfb3397437404b16220c26857037a05d
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 09:23:43 -0300

    class 3 - double

---
+++ 3-double/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Project 2 - Creating Variables");
+
+        double salary;
+
+        salary = 3000.13;
+
+        Console.WriteLine(salary);
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
