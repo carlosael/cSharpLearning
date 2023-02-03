8b289473d82641f35173c76220673937dedb3627
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 12:25:56 -0300

    class 9 - while

---
+++ P9-calcSavings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 9 - calculate savings");
+
+        double investment = 1000;
+
+        // 0.5% yield/month
+
+        Console.WriteLine(investment);
+
+        int month = 1;
+
+        while (month <= 12)
+        {
+            investment = investment + investment * 0.005;
+            Console.WriteLine("On the month" + month + "you will have R$ " + investment);
+            month++;
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
