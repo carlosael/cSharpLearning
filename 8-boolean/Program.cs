815957d90167b9e69a499592911eb39d875ee1f1
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 11:42:30 -0300

    class 8 - boolean

---
+++ 8-boolean/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 7 - conditionals");
+
+        int johnAge = 16;
+        bool group = true;
+
+        if (johnAge >= 18 || group)
+        {
+            Console.WriteLine("You may enter!");
+        }
+        else
+        {
+            Console.WriteLine("You cannot enter");
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
