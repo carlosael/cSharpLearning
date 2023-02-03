b1c2fe2d508a6f752cb0c6126e4392649a03cd86
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 12:40:11 -0300

    class 11 - using for

---
+++ p11-calcSavings2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 9 - calculate savings");
+
+        double investment = 1000;
+
+        // 0.5% yield/month
+
+        Console.WriteLine(investment);
+
+        for (int month = 1; month <= 12;month++) 
+        { 
+            investment *= 1.005;
+            Console.WriteLine("On the month " + month + " you will have R$ " + investment);
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
