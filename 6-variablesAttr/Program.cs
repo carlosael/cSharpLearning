916acb73cb164400f22b4f3667044994c39e31df
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-03 11:35:01 -0300

    class 7 - conditional

---
+++ 6-variablesAttr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 6 - variables attribution");
+
+        int age = 30;
+        int carlAge = age;
+
+        Console.WriteLine(carlAge);
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ 7-conditional/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 7 - conditionals");
+
+        int johnAge = 16;
+        int guests = 2;
+
+        if (johnAge >= 18)
+        {
+            Console.WriteLine("You may enter!");
+        }
+        else
+        {
+            if (guests > 0)
+            {
+                Console.WriteLine("You're with someone. You may enter!");
+            }
+            else
+            {
+                Console.WriteLine("You cannot enter");
+            }
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
