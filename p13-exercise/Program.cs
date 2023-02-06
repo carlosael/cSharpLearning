faf0abea998ff4defecea10d675547685d531dbf
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-06 14:32:43 -0300

    exercise 13 - multiples of 3

---
+++ p13-exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 13 - for chaining");
+
+        for (int x = 0; x < 100; x++)
+        {
+            if (x % 3 == 0)
+            {
+                Console.WriteLine(x);
+            }
+              
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
