ac129456a7fa6678df533a488919fb57d8ee9a3b
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-06 14:08:21 -0300

    class 13 - using break

---
+++ p13-forChaining/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 13 - for chaining");
+
+        for (int lineCounter = 0; lineCounter <= 9; lineCounter++)
+        {
+            for (int columnCounter = 0;columnCounter <= 9; columnCounter++)
+            {
+                Console.Write("*");
+                if (columnCounter >= lineCounter)
+                    break;
+            }
+           Console.WriteLine();
+        }
+
+        for (int lineCounter = 0; lineCounter <= 9; lineCounter++)
+        {
+            for (int columnCounter = 0; columnCounter <= lineCounter; columnCounter++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
