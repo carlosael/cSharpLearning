01e8cf850efdb5143001dbf01908d5635603fae6
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-06 15:32:44 -0300

    exercise 14 - factorial

---
+++ p14 - factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 13 - for chaining");
+        int result = 1;
+        for (int x = 0; x <= 10; x++)
+        {
+            if (x == 0 || x == 1)
+            {
+                Console.WriteLine("The factorial of " + x + " is 1");
+            } else
+            {
+                result = x * result;
+                Console.WriteLine("The factorial of " + x + " is " + result);
+            }
+
+        }
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
