f266684e94c32c8f92395520776c132af08d3cd8
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-06 13:54:52 -0300

    class 12 - working with for

---
+++ p12-longTermInvestments/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executing project 12 - long term investment");
+
+        double investmentRate = 1.005;
+        double investmentValue = 1000;
+
+
+        for (int years = 1; years <= 5; years++)
+        {
+            for (int month = 1; month <= 12;month++)
+            {
+                investmentValue *= investmentRate;
+            }
+            investmentRate += 0.001;
+        }
+
+        Console.WriteLine("After 5 years you will have R$ " + investmentValue);
+
+        Console.WriteLine("Type 'Enter' to close.");
+
+        Console.ReadLine();
+    }
+}
